e957e41908c79a13cde28ee338c051d8ac0fff12
Lindsay <lbarnett712@gmail.com>
2021-05-05 06:54:03 -0400

    getting data from static file

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import Layout from '../components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function HomePage() {
+export default function HomePage({ events }) {
+  console.log('events', events);
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  console.log('running getStaticProps...');
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  console.log(events);
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
