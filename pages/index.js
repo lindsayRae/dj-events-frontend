1335b0b9b3e21d4dacbb872a163d3e0174bb3e46
Lindsay <lbarnett712@gmail.com>
2021-05-05 07:26:10 -0400

    created all events page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import Layout from '../components/Layout';
+import Link from 'next/link';
+import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
 import { API_URL } from '@/config/index';
 
 export default function HomePage({ events }) {
@@ -6,6 +8,17 @@ export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className='btn-secondary'>View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
@@ -15,10 +28,10 @@ export async function getStaticProps() {
   const res = await fetch(`${API_URL}/api/events`);
   const events = await res.json();
 
-  console.log(events);
+  // console.log(events);
 
   return {
-    props: { events },
+    props: { events: events.slice(0, 3) },
     revalidate: 1,
   };
 }
