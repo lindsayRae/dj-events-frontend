19f4de7d5ed6b70891694a6c79b2d3f2a5a3982c
Lindsay <lbarnett712@gmail.com>
2021-05-13 08:03:29 -0400

    feat: added UserContext

--- pages/account/register.js
+++ pages/account/register.js
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { useState, useEffect, useContext } from 'react';
 import Link from 'next/link';
 import Layout from '@/components/Layout';
+import AuthContext from '@/context/AuthContext';
 import styles from '@/styles/AuthForm.module.css';
 
 export default function RegisterPage() {
@@ -12,12 +13,14 @@ export default function RegisterPage() {
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
 
+  const { register, error } = useContext(AuthContext);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (password !== passwordConfirm) {
       toast.error('Passwords do not match');
     }
-    console.log({ username, email, password });
+    register({ username, email, password });
   };
 
   return (
