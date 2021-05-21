2e7da31de2aea9c0efe9ceffa12d7b783eb3f5c6
Lindsay <lbarnett712@gmail.com>
2021-05-21 07:59:46 -0400

    fixed jtw spelling

--- pages/api/register.js
+++ pages/api/register.js
@@ -26,7 +26,7 @@ export default async (req, res) => {
 
       res.setHeader(
         'Set-Cookie',
-        cookie.serialize('token', data.jtw, {
+        cookie.serialize('token', data.jwt, {
           httpOnly: true,
           secure: process.env.NODE_ENV !== 'development',
           maxAge: 60 * 60 * 24 * 7, // 1 week
